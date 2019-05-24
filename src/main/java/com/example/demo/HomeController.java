cf1a9c90a49eb201faf30e9051869a34512ce880
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-24 16:55:32 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/2")
+    public String index2(){
+        return "page2";
+    }
+
+    @RequestMapping("/3")
+    public String index3(){
+        return "page3";
+    }
+}
